dabf052f4138a121c6b93f230e3674b9c4e0f79d
Josh Jones <jones.joshua.d@gmail.com>
2025-03-29 09:48:56 -0500

    setup of user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # Dashboard route for authenticated users
+  get 'dashboard' => 'dashboard#index', as: :dashboard
+
   # Defines the root path route ("/")
   root "home#index"
 end
